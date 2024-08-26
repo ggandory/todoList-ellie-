794d42eb8f443941bcbdb1e1db792066b3249f34
허건 <braveman0823@gmail.com>
2024-08-26 17:22:55 +0900

    input넣는 칸 고정을 위한 수정

---
+++ .history/src/components/TodoList/TodoList_20240826171758.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import AddTodo from "../AddTodo/AddTodo"; //AddTodo 컴포넌트를 임포트 해줘야 한다.
+import Todo from "../Todo/Todo";
+import styles from "./TodoList.module.css";
+
+export default function TodoList({ filter }) {
+  const [todos, setTodos] = useState(() => readTodoFromLocalStorage()); //그러나 이 안에는 readTodosFromLocalStrorage로만 표현됨
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((t) => t.id !== deleted.id));
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  const filtered = getFilteredItems(todos, filter);
+
+  return (
+    <section className={styles.container}>
+      <ul className={styles.list}>
+        {filtered.map((item) => (
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
+        ))}
+      </ul>
+      <AddTodo onAdd={handleAdd} />
+    </section>
+  );
+}
+
+function readTodoFromLocalStorage() {
+  const todos = localStorage.getItem("todos");
+  return todos ? JSON.parse(todos) : [];
+}
+
+function getFilteredItems(todos, filter) {
+  if (filter === "all") {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
+}
